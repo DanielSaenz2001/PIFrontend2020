6131e681135d0583c272ab17bbb602c5572d6447
DanielSaenz2001 <daniel.saenz@upeu.edu.pe>
2020-07-28 17:35:34 -0500

    auida :c

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-
+app.listen(process.env.PORT || 8080);
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
+console.log('Console Listening')
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
